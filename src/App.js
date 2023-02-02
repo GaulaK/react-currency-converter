f9e93549e30d778976d53814d18c96ef51493037
Gauthier Portefaix <portefaixgauthier@gmail.com>
2023-02-02 18:13:55 +0100

    beta (convert work but no auto actualize)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import rates from "./assets/data/Rates";
 
 function App() {
+  const [firstValue, setFirstValue] = useState(0);
+  const [secondValue, setSecondValue] = useState(0);
+  const [firstCurrency, setFirstCurrency] = useState("EUR");
+  const [secondCurrency, setSecondCurrency] = useState("EUR");
+
+  const handleFirstValueChange = (event) => {
+    const value = event.target.value;
+    setFirstValue(value);
+    setSecondValue(
+      ((value * rates[secondCurrency]) / rates[firstCurrency]).toFixed(2)
+    );
+  };
+  const handleSecondValueChange = (event) => {
+    const value = event.target.value;
+    setSecondValue(value);
+    setFirstValue(
+      ((value * rates[firstCurrency]) / rates[secondCurrency]).toFixed(2)
+    );
+  };
+
+  const handleFirstCurrencyChange = (event) => {
+    const value = event.target.value;
+    setFirstCurrency(value);
+  };
+
+  const handleSecondCurrencyChange = (event) => {
+    const value = event.target.value;
+    setSecondCurrency(value);
+  };
+
+  const tabRates = Object.keys(rates);
+
+  console.log(rates);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>💵 Converter 💵</h1>
+        <div>
+          <input
+            type="text"
+            name="firstValue"
+            value={firstValue}
+            onChange={handleFirstValueChange}
+          ></input>
+          <select value={firstCurrency} onChange={handleFirstCurrencyChange}>
+            {tabRates.map((rate, index) => {
+              return (
+                <option key={index} value={rate}>
+                  {rate}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <input
+            type="text"
+            name="secondValue"
+            value={secondValue}
+            onChange={handleSecondValueChange}
+          ></input>
+          <select value={secondCurrency} onChange={handleSecondCurrencyChange}>
+            {tabRates.map((rate, index) => {
+              return (
+                <option key={index} value={rate}>
+                  {rate}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
